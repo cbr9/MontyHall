21ac39d118fa224c58925d13fa05b38bcd4afb84
cbr9 <cabero96@protonmail.com>
2019-12-10 20:24:54 +0100

    did some refactors and optimizations

--- MontyHall.py
+++ MontyHall.py
@@ -1,30 +1,36 @@
 import numpy as np
+from typing import Dict
 
 
 class MontyHall:
     
     def __init__(self, change_door: bool):
-        self.__doors = [0, 1, 2]
-        self.__prizes = [True, False, False]
-        np.random.shuffle(self.__doors)
-        np.random.shuffle(self.__prizes)
-        self.__prized_doors = dict(zip(self.__doors, self.__prizes))
-        self.__selection = np.random.choice(self.__doors)
-        self.__opened_door = self.open_door()
+        self.doors, self.prizes, self.correct_door = self.create_doors()
+        self.selection = self.open_door(change_door)
+
+    def check_result(self) -> bool:
+        if self.selection == self.correct_door:
+            return True
+        else:
+            return False
+
+    def create_doors(self) -> Dict:
+        doors = [0, 1, 2]
+        prizes = [True, False, False]
+        np.random.shuffle(doors)
+        np.random.shuffle(prizes)
+        return doors, prizes, prizes.index(True)
+
+
+    def open_door(self, change_door: bool) -> int:
+        selection = np.random.choice(self.doors)
         if change_door:
-            self.__selection = self.change_selection()
-        self.__correct_door = [door for door in self.__doors if self.__prized_doors[door] is True][0]
-        self.final = True if self.__selection == self.__correct_door else False
+            wrong_doors = [door for door in self.doors if self.prizes[door] is False and door != selection]
+            to_open = np.random.choice(wrong_doors)
+            self.doors.remove(to_open)
+            selection = np.random.choice([door for door in self.doors if door != selection])
 
-    def open_door(self) -> int:
-        wrong_doors = [door for door in self.__doors if self.__prized_doors[door] is False and door != self.__selection]
-        to_open = np.random.choice(wrong_doors)
-        self.__doors.remove(to_open)
-        return to_open
-
-    def change_selection(self) -> int:
-        new_selection = np.random.choice([door for door in self.__doors if door != self.__selection])
-        return new_selection
+        return selection
 
 
 if __name__ == "__main__":
@@ -32,7 +38,7 @@ if __name__ == "__main__":
     results = []
     for x in range(100000):
         test = MontyHall(change_door=True)
-        results.append(test.final)
+        results.append(test.check_result())
 
     print(f"Correct results having changed the selected door: {results.count(True)}")
     print(f"Incorrect results having changed the selected door: {results.count(False)}")
@@ -41,7 +47,7 @@ if __name__ == "__main__":
     results = []
     for x in range(100000):
         test = MontyHall(change_door=False)
-        results.append(test.final)
+        results.append(test.check_result())
 
     print(f"Correct results NOT having changed the selected door: {results.count(True)}")
     print(f"Incorrect results NOT having changed the selected door: {results.count(False)}")
